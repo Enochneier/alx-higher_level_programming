970de67dba6b4079d23249a126d7d2104b02e34b
Enochneier <enochneier@gail.com>
2024-01-25 11:14:13 -0800

    0. My first square

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan 25 2020
+@author: Enochneier
+"""
+
+
+class square:
+    """Empty class Square that defines a square
+
+    Attributes:
+        empty
+    """
+
+    pass
