8ca80775a281be0c807e0a7d90e9c1422b48b58a
Enochneier <enochneier@gail.com>
2024-01-27 00:12:58 -0800

    0. Simple rectangle

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jan 27 2024
+@author: Enochneier
+"""
+
+
+class Rectangle:
+    """Empty class Rectangle that defines a rectangle figure
+
+    Attributes:
+        empty
+    """
+
+    pass
