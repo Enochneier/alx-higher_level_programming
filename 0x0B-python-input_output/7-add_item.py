3d89fa7286aa509158735f9b0e439a6deee742d7
Enochneier <enochneier@gail.com>
2024-02-17 04:25:18 -0800

    corrections

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -6,8 +6,8 @@ Created on Sat Feb 17 2024
 @author: Enochneier
 """
 import sys
-save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
-load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 
 try:
