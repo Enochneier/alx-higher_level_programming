f74a76285ab8776ab8eb09386d03d61094a840b0
Enochneier <enochneier@gail.com>
2024-02-18 08:06:56 -0800

    0. Lookup

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Feb 18 2024
+
+@author: Enochneier
+"""
+
+
+def lookup(obj):
+    """
+    Returns all objects in an objects dictionary as a list
+    """
+    return dir(obj)
