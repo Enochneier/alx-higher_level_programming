9bade6ef66f170bfb412fc66c35d24f0a07d8735
Enochneier <enochneier@gail.com>
2024-02-19 02:09:44 -0800

    6. Improve Geometry

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb 19 2024
+
+@author:Enochneier
+"""
+
+
+class BaseGeometry():
+    """
+    An empty class
+    """
+    pass
+
+    def area(self):
+        """
+        Public instance method that calculates the area
+
+        Raises:
+            Exception if area is not imlemented
+        """
+        raise Exception("area() is not implemented")
